2b04e48768075b7fabbf0f78c42f4c760dc15126
Pserg <peleserg@gmail.com>
2019-07-02 09:05:45 +0500

    [+] parseFullName

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,2 +1,10 @@
 package ru.skillbranch.devintensive.utils
 
+object Utils {
+    fun parseFullName(fullName : String?) : Pair<String?, String?> {
+        val parts : List<String>? = fullName?.split(" ")
+        val firstName = if (parts?.getOrNull(0) == "") "null" else parts?.getOrNull(0)
+        val lastName = if (parts?.getOrNull(1) == "") "null" else parts?.getOrNull(1)
+        return firstName to lastName
+    }
+}
