86cba9279e528668dabff42bcb33f05c5689a59a
Pserg <peleserg@gmail.com>
2019-07-02 12:46:35 +0500

    [+] transliteration

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -13,4 +13,82 @@ object Utils {
         val secondInitial = if (lastName?.trim()?.length != 0) lastName?.trim()?.substring(0, 1) else null
         return if (firstInitial == null && secondInitial == null) null else (firstInitial ?: "") + (secondInitial ?: "")
     }
+
+    fun transliteration(payload : String?, divider : String = " ") : String? {
+        if (payload == null) return null
+        var result = ""
+        payload.forEach { char ->
+            result += when (char) {
+                ' ' -> divider
+                'а' -> "a"
+                'б' -> "b"
+                'в' -> "v"
+                'г' -> "g"
+                'д' -> "d"
+                'е' -> "e"
+                'ё' -> "e"
+                'ж' -> "zh"
+                'з' -> "z"
+                'и' -> "i"
+                'й' -> "i"
+                'к' -> "k"
+                'л' -> "l"
+                'м' -> "m"
+                'н' -> "n"
+                'о' -> "o"
+                'п' -> "p"
+                'р' -> "r"
+                'с' -> "s"
+                'т' -> "t"
+                'у' -> "u"
+                'ф' -> "f"
+                'х' -> "h"
+                'ц' -> "c"
+                'ч' -> "ch"
+                'ш' -> "sh"
+                'щ' -> "sh'"
+                'ъ' -> ""
+                'ы' -> "i"
+                'ь' -> ""
+                'э' -> "e"
+                'ю' -> "yu"
+                'я' -> "ya"
+                'А' -> "A"
+                'Б' -> "B"
+                'В' -> "V"
+                'Г' -> "G"
+                'Д' -> "D"
+                'Е' -> "E"
+                'Ё' -> "E"
+                'Ж' -> "Zh"
+                'З' -> "Z"
+                'И' -> "I"
+                'Й' -> "I"
+                'К' -> "K"
+                'Л' -> "L"
+                'М' -> "M"
+                'Н' -> "N"
+                'О' -> "O"
+                'П' -> "P"
+                'Р' -> "R"
+                'С' -> "S"
+                'Т' -> "T"
+                'У' -> "U"
+                'Ф' -> "F"
+                'Х' -> "H"
+                'Ц' -> "C"
+                'Ч' -> "Ch"
+                'Ш' -> "Sh"
+                'Щ' -> "Sh'"
+                'Ъ' -> ""
+                'Ы' -> "I"
+                'Ь' -> ""
+                'Э' -> "E"
+                'Ю' -> "Yu"
+                'Я' -> "Ya"
+                else -> char.toString()
+            }
+        }
+        return result
+    }
 }
