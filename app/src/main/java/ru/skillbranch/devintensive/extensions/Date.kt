c71cdd430f6da8e4ab6d838713d8afda0fa9ed2b
Pserg <peleserg@gmail.com>
2019-07-02 11:06:36 +0500

    [+] toInitials

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -14,13 +14,6 @@ fun Date.format(pattern : String = "HH:mm:ss dd.MM.yy") : String {
     return dateFormat.format(this)
 }
 
-//Реализуй extension Date.add(value, TimeUnits) добавляющий или вычитающий значение переданное первым аргументом
-//в единицах измерения второго аргумента (enum TimeUnits [SECOND, MINUTE, HOUR, DAY]) и возвращающий
-//модифицированный экземпляр Date
-//Пример:
-//Date().add(2, TimeUnits.SECOND) //Thu Jun 27 14:00:02 GST 2019
-//Date().add(-4, TimeUnits.DAY) //Thu Jun 23 14:00:00 GST 2019
-
 fun Date.add(value : Int, units : String) : Date {
     var time = this.time
     time += when (units) {
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -7,4 +7,10 @@ object Utils {
         val lastName = if (parts?.getOrNull(1) == "") null else parts?.getOrNull(1)
         return firstName to lastName
     }
+
+    fun toInitials(firstName : String?, lastName : String?) : String? {
+        val firstInitial = if (firstName?.trim()?.length != 0) firstName?.trim()?.substring(0, 1) else null
+        val secondInitial = if (lastName?.trim()?.length != 0) lastName?.trim()?.substring(0, 1) else null
+        return if (firstInitial == null && secondInitial == null) null else (firstInitial ?: "") + (secondInitial ?: "")
+    }
 }
