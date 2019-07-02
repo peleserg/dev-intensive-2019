51142278a3b93987ac713eca90ce0b84b3437dcf
Pserg <peleserg@gmail.com>
2019-07-02 09:15:42 +0500

    [+] Date.format

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,6 +1,18 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.text.SimpleDateFormat
 import java.util.*
+
+//Реализуй extension Date.format(pattern) возвращающий отформатированную дату по паттерну передаваемому в качестве
+//аргумента (значение по умолчанию "HH:mm:ss dd.MM.yy" локаль "ru")
+//Пример:
+//Date().format() //14:00:00 27.06.19
+//Date().format("HH:mm") //14:00
+
+fun Date.format(pattern : String = "HH:mm:ss dd.MM.yy") : String {
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
 
 fun Date.humanizeDiff() : String {
     return ""
