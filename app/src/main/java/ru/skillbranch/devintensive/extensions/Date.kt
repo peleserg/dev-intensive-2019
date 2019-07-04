e45c0d64d2007a09b6140c2767cd1885c1df35ac
Pserg <peleserg@gmail.com>
2019-07-04 17:30:38 +0500

    [+] plural

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -31,39 +31,15 @@ fun Date.add(value : Int, units : TimeUnits) : Date {
 
 fun Date.humanizeDiff() : String {
     val delta = Date().time - this.time
-    val minutes = ceil(1.0 * delta.absoluteValue / MINUTE).toInt()
-    val hours = ceil(1.0 * delta.absoluteValue / HOUR).toInt()
-    val days = ceil(1.0 * delta.absoluteValue / DAY).toInt()
     val deltaString = when (delta.absoluteValue) {
         in 0 .. 1 * SECOND -> "только что"
         in 1 * SECOND .. 45 * SECOND -> "несколько секунд"
         in 45 * SECOND .. 75 * SECOND -> "минуту"
-        in 75 * SECOND .. 45 * MINUTE -> when(minutes % 100) {
-            in 11 .. 19 -> "$minutes минут"
-            else -> when(minutes % 10) {
-                1 -> "$minutes минуту"
-                in 2 .. 4 -> "$minutes минуты"
-                else -> "$minutes минут"
-            }
-        }
+        in 75 * SECOND .. 45 * MINUTE -> TimeUnits.MINUTE.plural(ceil(1.0 * delta.absoluteValue / MINUTE).toInt())
         in 45 * MINUTE .. 75 * MINUTE -> "час"
-        in 75 * MINUTE .. 22 * HOUR -> when(hours % 100) {
-            in 11 .. 19 -> "$hours часов"
-            else -> when(hours % 10) {
-                1 -> "$hours час"
-                in 2 .. 4 -> "$hours часа"
-                else -> "$hours часов"
-            }
-        }
+        in 75 * MINUTE .. 22 * HOUR -> TimeUnits.HOUR.plural(ceil(1.0 * delta.absoluteValue / HOUR).toInt())
         in 22 * HOUR .. 26 * HOUR -> "день"
-        in 26 * HOUR .. 360 * DAY -> when(days % 100) {
-            in 11 .. 19 -> "$days дней"
-            else -> when(days % 10) {
-                1 -> "$days день"
-                in 2 .. 4 -> "$days дня"
-                else -> "$days дней"
-            }
-        }
+        in 26 * HOUR .. 360 * DAY -> TimeUnits.DAY.plural(ceil(1.0 * delta.absoluteValue / DAY).toInt())
         else -> "что-то не так со временем"
     }
     if (delta < -360 * DAY) return "более чем через год"
@@ -73,8 +49,37 @@ fun Date.humanizeDiff() : String {
 }
 
 enum class TimeUnits {
-    SECOND,
-    MINUTE,
-    HOUR,
-    DAY
+    SECOND {
+        override fun plural(value : Int) : String {
+            return pluralSelect(value, "секунду", "секунды", "секунд")
+        }
+    },
+    MINUTE {
+        override fun plural(value : Int) : String {
+            return pluralSelect(value, "минуту", "минуты", "минут")
+        }
+    },
+    HOUR {
+        override fun plural(value : Int) : String {
+            return pluralSelect(value, "час", "часа", "часов")
+        }
+    },
+    DAY {
+        override fun plural(value : Int) : String {
+            return pluralSelect(value, "день", "дня", "дней")
+        }
+    };
+
+    abstract fun plural(value : Int) : String
+
+    fun pluralSelect(value : Int, one : String, few : String, many : String) : String {
+        return when(value % 100) {
+            in 11 .. 19 -> "$value $many"
+            else -> when(value % 10) {
+                1 -> "$value $one"
+                in 2 .. 4 -> "$value $few"
+                else -> "$value $many"
+            }
+        }
+    }
 }
